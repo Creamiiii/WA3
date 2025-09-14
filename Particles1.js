ab1ab97f186a8f4bfa0e8fbe0c0caea0e75cc645
Creamiiii <stillnothing4now@gmail.com>
2025-09-14 20:30:02 +0800

    Update Particles1.js

--- Particles1.js
+++ Particles1.js
@@ -4,8 +4,9 @@ class Particle1 {
     this.y = 300;
   }
   iterate() {
-    this.x = (this.x <= 400) ? this.x + EASY_MODE.v1 / 5 : this.x + EASY_MODE.v2 / 5;
+    this.x =
+      this.x <= 400 ? this.x + EASY_MODE.v1 / 5 : this.x + EASY_MODE.v2 / 5;
     fill(0);
-    ellipse(this.x, this.y, 2,2);
+    ellipse(this.x, this.y, 2, 2);
   }
 }
